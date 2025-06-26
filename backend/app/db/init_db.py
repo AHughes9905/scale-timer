fb320137dc417af49b1836115d004f71b9f19ef1
Hughes, Aaron John <ahughes9905@gmail.com>
2025-06-26 11:43:16 -0600

    setup backend dockerfile

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -1,11 +1,23 @@
 import asyncio
+from sqlalchemy import inspect
 from models import user, timing_log
 from db.database import Base, engine
 
 
-async def init_models():
+async def is_initialized():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+        inspector = inspect(conn)
+        tables = await conn.run_sync(inspector.get_table_names)
+        return "users" in tables
+
+
+async def init_models():
+    if not await is_initialized():
+        print("Initializing database...")
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+    else:
+        print("Database already initialized.")
 
 
 if __name__ == "__main__":
